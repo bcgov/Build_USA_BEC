9a48af9162f5703a4c260176b11a17f1d03fa490
whmacken <will.mackenzie@gov.bc.ca>
2023-11-14 07:11:37 -0800

    Comment out vars that were not used in the end

--- _functions/AddVars.R
+++ _functions/AddVars.R
@@ -1,3 +1,4 @@
+dat = training
 addVars <- function(dat){
   dat$PPT_MJ <- dat$PPT05 + dat$PPT06  # MaY/June precip
   dat$PPT_JAS <- dat$PPT07 + dat$PPT08 + dat$PPT09  # July/Aug/Sept precip
@@ -6,31 +7,31 @@ addVars <- function(dat){
   dat$CMD.def[dat$CMD.def < 0] <- 0  #negative values set to zero = no deficit
   dat$CMDMax <- dat$CMD07
   dat$CMD.total <- dat$CMD.def + dat$CMD
-  dat$CMD.grow <- dat$CMD05 + dat$CMD06 +dat$CMD07 +dat$CMD08 +dat$CMD09
-  dat$DD5.grow <- dat$DD5_05 + dat$DD5_06 + dat$DD5_07 + dat$DD5_08 + dat$DD5_09
+  # dat$CMD.grow <- dat$CMD05 + dat$CMD06 +dat$CMD07 +dat$CMD08 +dat$CMD09
+  # dat$DD5.grow <- dat$DD5_05 + dat$DD5_06 + dat$DD5_07 + dat$DD5_08 + dat$DD5_09
   dat$CMDMax <- dat$CMD07 # add in so not removed below
-  dat$DDgood <- dat$DD5 - dat$DD18
-  dat$DDnew <- (dat$DD5_05 + dat$DD5_06 +dat$DD5_07  + dat$DD5_08)  - (dat$DD18_05 + dat$DD18_06 +dat$DD18_07 +dat$DD18_08)
-  dat$TmaxJuly <- dat$Tmax07
-  dat$tmaxSum20 <-  dat %>%
-    select( starts_with("Tmax")) %>% -20 %>%
-    mutate_all(., list(~ifelse(. < 0, 0 , .))) %>%
-    rowSums(na.rm = TRUE)
-  
-  dat$tmaxSum25 <-  dat %>%
-    select( starts_with("Tmax")) %>% -25 %>%
-    mutate_all(., list(~ifelse(. < 0, 0 , .))) %>%
-    rowSums(na.rm = TRUE)
-  
-  dat$tmaxSum30 <-  dat %>%
-    select( starts_with("Tmax")) %>% -30 %>%
-    mutate_all(., list(~ifelse(. < 0, 0 , .))) %>%
-    rowSums(na.rm = TRUE)
-  
-  dat$tmaxSum35 <-  dat %>%
-    select( starts_with("Tmax")) %>% -35 %>%
-    mutate_all(., list(~ifelse(. < 0, 0 , .))) %>%
-    rowSums(na.rm = TRUE)
+  # dat$DDgood <- dat$DD5 - dat$DD18
+  # dat$DDnew <- (dat$DD5_05 + dat$DD5_06 +dat$DD5_07  + dat$DD5_08)  - (dat$DD18_05 + dat$DD18_06 +dat$DD18_07 +dat$DD18_08)
+  # dat$TmaxJuly <- dat$Tmax07
+  # dat$tmaxSum20 <-  dat %>%
+  #   select( starts_with("Tmax")) %>% -20 %>%
+  #   mutate_all(., list(~ifelse(. < 0, 0 , .))) %>%
+  #   rowSums(na.rm = TRUE)
+  # 
+  # dat$tmaxSum25 <-  dat %>%
+  #   select( starts_with("Tmax")) %>% -25 %>%
+  #   mutate_all(., list(~ifelse(. < 0, 0 , .))) %>%
+  #   rowSums(na.rm = TRUE)
+  # 
+  # dat$tmaxSum30 <-  dat %>%
+  #   select( starts_with("Tmax")) %>% -30 %>%
+  #   mutate_all(., list(~ifelse(. < 0, 0 , .))) %>%
+  #   rowSums(na.rm = TRUE)
+  # 
+  # dat$tmaxSum35 <-  dat %>%
+  #   select( starts_with("Tmax")) %>% -35 %>%
+  #   mutate_all(., list(~ifelse(. < 0, 0 , .))) %>%
+  #   rowSums(na.rm = TRUE)
   
   dat <- dat %>%  mutate(DD_delayed = (((DD_0_at+ DD_0_wt )*0.0238) - 1.8386)) %>% mutate_if(is.numeric, round, digits=2)
   dat$DD_delayed <- ifelse(dat$DD_delayed <=0, 0, dat$DD_delayed )
